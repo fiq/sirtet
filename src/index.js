3d6b0b7eb462a7dc14e2ef65969fbf07dac35c2d
Raf Gemmail <rafiq@dreamthought.com>
2019-02-07 15:09:12 +1300

    Initial scene setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import SirTet from "./js/components/container/SirTet.jsx";
+import SirTet from "./js/components/container/SirTet.jsx"
+import { BrowserRouter } from 'react-router-dom'
+import React from "react"
+
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <SirTet/>
+  </BrowserRouter>
+, document.getElementById('sirtet-game-board'))
--- src/js/components/container/SirTet.jsx
+++ src/js/components/container/SirTet.jsx
@@ -1,14 +1,17 @@
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
 
-class SirTet extends Comopnent {
+import React, { Component } from "react"
+
+class SirTet extends Component {
     constructor() {
         super();
     }
 
-    render () {
-        <a-scene>
-
-        </a-scene>
+    render() {
+        return (
+            <a-scene>
+                <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+            </a-scene>
+        )
     }
 }
+export default SirTet;
