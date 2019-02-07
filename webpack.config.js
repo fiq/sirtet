a20c03cb8f4cddda9411f38cf37b836e80551ea3
Raf Gemmail <rafiq@dreamthought.com>
2019-02-07 13:39:44 +1300

    Added webpack and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
     module: {
         rules: [
             {
-                test: /.jsx?$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.html*$/,
+                use : [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+        })
+    ]
 };
