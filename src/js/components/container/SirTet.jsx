41c2c7da197a7479959ab06a59aa90cd851676a6
Raf Gemmail <rafiq@dreamthought.com>
2019-02-07 13:38:14 +1300

    Added entrypoint component

---
+++ src/js/components/container/SirTet.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+class SirTet extends Comopnent {
+    constructor() {
+        super();
+    }
+
+    render () {
+        <a-scene>
+
+        </a-scene>
+    }
+}
