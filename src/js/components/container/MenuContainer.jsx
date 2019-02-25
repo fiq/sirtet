862d23f284bc25a82392c7adf639726433346765
Raf Gemmail <rafiq@dreamthought.com>
2019-02-26 11:55:28 +1300

    Added menu component

---
+++ src/js/components/container/MenuContainer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+
+class MenuContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <b>Menu</b>
+        )
+    }
+}
+
+export default MenuContainer;   
